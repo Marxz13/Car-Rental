f65200ff18debdc4ed0ed74a2718b35206db13b6
Marz <141631535+Marxz13@users.noreply.github.com>
2023-10-08 15:13:39 +0800

    Delete CarRental directory

--- CarRental/login.js
+++
@@ -1,6 +0,0 @@
-window.onbeforeunload = function() { // attach an event handler 
-    window.setTimeout(function () { // set a timeout 
-        window.location = 'login.php'; // redirect the user to the login.php page
-    }, 0); // redirect will happen immediately
-    window.onbeforeunload = null; 
-}
